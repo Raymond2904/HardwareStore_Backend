b2eb657ce364a09892fb0dd2df203e4f21ee6f8c
rupert van niekerk <127295503+rupert08@users.noreply.github.com>
2024-08-27 19:18:33 +0200

    initial file commit

---
+++ src/test/java/za/ac/cput/service/CustomerServiceTest.java
@@ -0,0 +1,70 @@
+package za.ac.cput.service;
+
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import za.ac.cput.domain.Address;
+import za.ac.cput.domain.Contact;
+import za.ac.cput.domain.Customer;
+import za.ac.cput.factory.AddressFactory;
+import za.ac.cput.factory.ContactFactory;
+import za.ac.cput.factory.CustomerFactory;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class CustomerServiceTest {
+
+    @Autowired
+    private CustomerService customerService;
+
+    private static final Contact contact = ContactFactory.createContact("testCust1@gmail.com", "02189456123");
+    private static final Address address = AddressFactory.createAddress("12", "Test Street", "Grabouw", "Western Cape", "7160");
+
+    private static final Customer customer = CustomerFactory.createCustomer(
+            "testCust@gmail.com", "John", "Wick", contact, "123456",  Collections.singletonList(address));
+
+    @Test
+    @Order(1)
+    void a_create() {
+        Customer created = customerService.create(customer);
+        assertNotNull(created);
+        System.out.println(created);
+    }
+
+    @Test
+    @Order(2)
+    void b_read() {
+        Customer read = customerService.read(customer.getUserId());
+        assertNotNull(read);
+        System.out.println(read);
+    }
+
+    @Test
+    @Order(3)
+    void c_update() {
+        Customer newCustomer = customer.toBuilder()
+                .firstName("Johnathan")
+                .build();
+        Customer updated = customerService.update(newCustomer);
+        assertNotNull(updated);
+        System.out.println(updated);
+    }
+
+    @Test
+    @Disabled
+    @Order(4)
+    void d_delete() {
+        customerService.delete(customer.getUserId());
+        System.out.println("Customer Deleted Where User ID = " + customer.getUserId());
+    }
+
+    @Test
+    @Order(5)
+    void e_getAll() {
+        System.out.println(customerService.getAll());
+    }
+}
